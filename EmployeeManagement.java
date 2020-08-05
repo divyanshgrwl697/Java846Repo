ad1f7677cc6adbfa9dcbe9bf8aaeab47c290a078
divyanshgrwl697 <69237455+divyanshgrwl697@users.noreply.github.com>
2020-08-05 15:04:02 +0530

    product function added

--- EmployeeManagement.java
+++ EmployeeManagement.java
@@ -5,4 +5,7 @@ class Calculator{
    public void div(int a, int b){
       System.out.println("Difference is: " + (a-b));
    }
+   public void product(int a, int b){
+      System.out.println("Product is: " + (a*b));
+   }
 }
